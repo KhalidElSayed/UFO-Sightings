2bd7be4fb75a7fb6ea173ff2c48747d9b9a50e87
Richard Kirk <RichardBKirk@gmail.com>
2012-04-28 18:43:34 -0500

    Initial Commit

---
+++ UfoSightings/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  UfoSightings
+//
+//  Created by Richard Kirk on 4/28/12.
+//  Copyright (c) 2012 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
